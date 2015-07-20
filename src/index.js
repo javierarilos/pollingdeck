3984b882244344c4f3ac3a1880c30ebf27836ea7
javierarilos <javier.arilos@gmail.com>
2015-07-21 00:07:30 +0200

    ongoing: implementing a simple http-request-router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var path = require('path');
+var parseUrl = require('url').parse;
 
 function getPolls(){
     return [
@@ -77,29 +78,155 @@ function updateClients(responses, object) {
     })
 }
 
+function httpNotFound(req, res) {
+    res.writeHead(404);
+    res.end();
+}
+
+function decorateWithBody(func, req, res) {
+    var body = '';
+    req.on('data', function(chunk) {
+        body += chunk;
+    });
+
+    req.on('end', function() {
+        req.json = JSON.parse(body);
+        req.body = body;
+        func(req, res);
+    });
+    return function(){};
+
+}
+
+var router = {
+    getRoutes: [],
+    postRoutes: [],
+    post: function(path, func) {
+        this.postRoutes.push({path: path, func: func})
+    },
+    get: function(path, func) {
+        this.getRoutes.push({path: path, func: func})
+    },
+    routeFor: function(req, res){
+        var route;
+        switch (req.method) {
+            case 'GET':
+                var routes = this.getRoutes;
+                route = this.findMatch(routes, req);
+                break;
+            case 'POST':
+                routes = this.postRoutes;
+
+                var funcToApply = this.findMatch(routes, req);
+                console.log(' ++ funcToApply', funcToApply.name);
+                route = decorateWithBody(funcToApply, req, res);
+                break;
+        }
+
+        return route;
+
+    },
+    findMatch: function(routes, req){
+        var url = req.parsedUrl.pathname;
+        console.log('>>>> req.parsedUrl', url, req.method);
+        var route = httpNotFound;
+        var currRoute;
+        //return this.getRoutes[0].func; //TODO: IMPLEMENT.
+        for(var i = 0; i < routes.length; i ++){
+            currRoute = routes[i];
+            console.log('checking:', url, 'against:', currRoute.path);
+            if (currRoute.path === url) {
+                console.log('Found route!');
+                route = currRoute.func;
+                break;
+            }
+        }
+        return route;
+    },
+    route: function() {
+        return function(req, res) {
+            req.parsedUrl = parseUrl(req.url, true);
+            var urlPath = req.parsedUrl.pathname;
+            var func = this.routeFor(req, res);
+            func(req, res);
+        }.bind(this);
+    }
+};
+
+function getIndex(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    var page = fs.readFileSync(path.join(__dirname, 'page.html'), {encoding: 'utf8'});
+
+    if(req.parsedUrl.query.presenter === 'secret') {
+        console.log('========>>> generating presenter page.');//TODO: templating?
+        presenterId = ""+Date.now();
+        page = page.replace('<!-- presenter-id -->', presenterId);
+        //TODO: render an html piece in a page.
+        var presenterButtonsHtml =  '<input type="button" id="page-prev" class="response-btn" name="prev" value="< prev" onclick="submitPagingRequest(\'prev\')">' +
+            '<input type="button" id="page-next" class="response-btn" name="next" value="next >" onclick="submitPagingRequest(\'next\')">';
+        page = page.replace('<!-- presenter-sect -->', presenterButtonsHtml);
+    } else {
+        console.log('========>>> generating user page.');
+        users += 1;
+        page = page.replace('<!-- presenter-id -->', 'happy-user-'+users);
+    }
+
+    return res.end(page);
+}
+
+function getPoll(req, res) {
+    clientResponses.push(res);
+    console.log('>>>>', req.url, req.headers);
+    res.writeHead(200, {'Content-Type': 'text/event-stream'});
+
+    updateClients(clientResponses, getCurrentPoll());
+    return;
+}
+
+function postResponse (req, res) {
+    var poll = getCurrentPoll();
+    var pollResponse = req.json;
+    console.log('***** response: question:', pollResponse.question, 'response:', pollResponse.response);
+    if(typeof pollResponse.question === 'number' && typeof pollResponse.response === 'number' ) {
+        console.log('***** correct response: question:', pollResponse.question, 'response:', pollResponse.response);
+
+        if(poll.id === pollResponse.question && pollResponse.response <= poll.responses.length - 1) {
+            console.log('*** question and response found. counting.');
+            poll.responses[pollResponse.response].count += 1;
+            updateClients(clientResponses, poll);
+            res.writeHead(200, "OK", {'Content-Type': 'text/html'});
+        } else {
+            var msg = "Not found: curr poll: "+poll.id+" max response: "+ poll.responses.length -1;
+            console.log('*** '+ msg);
+            res.writeHead(404, msg, {'Content-Type': 'text/html'});
+        }
+
+    } else {
+        var msg = ">>> could not parse question correctly :-(";
+        console.log('====' + msg);
+        res.writeHead(400, msg, {'Content-Type': 'text/html'});
+    }
+    res.end();
+
+    return;
+
+}
+
+
+router.get('/', getIndex);
+router.get('/index.html', getIndex);
+router.get('/poll', getPoll);
+router.post('/response', postResponse);
+
+http.createServer(router.route()).listen(8126, "0.0.0.0");
+
 http.createServer(function (req, res) {
 
     var parsedUrl = require('url').parse(req.url, true);
+    req.parsedUrl = parseUrl(req.url, true);
     var urlPath = parsedUrl.pathname;
     if (urlPath == '/' || urlPath.indexOf('index.html') !== -1) {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        var page = fs.readFileSync(path.join(__dirname, 'page.html'), {encoding: 'utf8'});
-
-        if(parsedUrl.query.presenter === 'secret') {
-            console.log('========>>> generating presenter page.');//TODO: templating?
-            presenterId = ""+Date.now();
-            page = page.replace('<!-- presenter-id -->', presenterId);
-            //TODO: render an html piece in a page.
-            var presenterButtonsHtml =  '<input type="button" id="page-prev" class="response-btn" name="prev" value="< prev" onclick="submitPagingRequest(\'prev\')">' +
-                                        '<input type="button" id="page-next" class="response-btn" name="next" value="next >" onclick="submitPagingRequest(\'next\')">';
-            page = page.replace('<!-- presenter-sect -->', presenterButtonsHtml);
-        } else {
-            console.log('========>>> generating user page.');
-            users += 1;
-            page = page.replace('<!-- presenter-id -->', 'happy-user-'+users);
-        }
-
-        return res.end(page);
+        getIndex(req, res);
     }
 
 
