cf2dfdc8634194cf9014f11b1f7d1f266bd78602
javierarilos <javier.arilos@gmail.com>
2015-08-14 08:10:07 +0200

    add session accessors to manager

--- src/lib/sessionManager.js
+++ src/lib/sessionManager.js
@@ -7,4 +7,20 @@ function newPresenterSession(user) {
     return sessionId;
 }
 
+function getSession(sessionId) {
+    return presenterSessions[sessionId];
+}
+
+function getSessionByUser(user) {
+    var session = null;
+    for (var sessionId in presenterSessions) {
+        session = presenterSessions[sessionId];
+        if(session.user === user) {
+            break;
+        }
+    }
+    return session;
+}
 module.exports.newPresenterSession = newPresenterSession;
+module.exports.getSession = getSession;
+module.exports.getSessionByUser = getSessionByUser;
