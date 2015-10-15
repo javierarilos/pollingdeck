8123a74381c81b159cc949b466fda56d9bc1e188
javierarilos <javier.arilos@gmail.com>
2015-10-15 23:36:55 +0200

    test on notifications: one subscriber x 500 responses.

--- test/load.test.js
+++ test/load.test.js
@@ -56,8 +56,54 @@ suite('API server', function() {
               console.log("<<Received response. still pending: ",pending);
               if (pending <= 0) done();
           });
-
       }
+    });
+
+    suite('SUBSCRIBE', function () {
+      setup(function(done) {
+        hippie()
+          .header("User-Agent", "hippie")
+          .get('http://'+host+'/?user=secret&pass=pass')
+          .expectStatus(200)
+          .end(function(err, res, body) {
+              if (err) throw err;
+              done();
+          });
+      });
+
+      test('should receive responses via SSE', function (done) {
+        var total = 500;
+
+        var pending_responses = total;
+        var pending_notifications = total + 1; // notifications responses + 1 on subscribe
+
+        var EventSource = require('eventsource');
+
+        var es = new EventSource('http://'+host+'/poll?tout=10000');
+        es.onmessage = function(e) {
+          pending_notifications -=1
+          console.log(">>>>>>>>>>>>>>>>=======>>>> event received. pending: ", pending_notifications);
+          if (pending_notifications === 0) done();
+        };
+        es.onerror = function() {
+          console.log('ERROR!');
+        };
+        setTimeout(function(){
+          for(var i = 0; i < total; i++) {
+            console.log(">>About to send. still pending: ", pending_responses);
+            hippie()
+              .post('http://'+host+'/response')
+              .send('{"question": 0, "response": 2}')
+              .expectStatus(200)
+              .end(function(err, res, body) {
+                  if (err) throw err;
+                  pending_responses -= 1;
+                  console.log("<<Received response. still pending: ", pending_responses);
+              });
+          }
+
+        }, 500);
+      });
     });
   });
 });
