48fc43a4be5577d64d5062fa907888f94e52ff0a
javierarilos <javier.arilos@gmail.com>
2015-10-23 07:04:01 +0200

    created new poll questions.

--- src/lib/pollsProvider.js
+++ src/lib/pollsProvider.js
@@ -24,7 +24,7 @@ var defaultPollDefinitions = [
                     },
                     {
                         id: 2,
-                        text: "Not but allows coding everywhere.",
+                        text: "Not but allows coding everywhere",
                         count: 0
                     },
                     {
@@ -36,7 +36,7 @@ var defaultPollDefinitions = [
             },
             {
                 type: 'update',
-                title: 'JS, Python or Java?',
+                title: 'If you could choose...',
                 id: 1,
                 responses: [
                     {
@@ -46,19 +46,73 @@ var defaultPollDefinitions = [
                     },
                     {
                         id: 1,
-                        text: "Python, batteries included.",
+                        text: "Python, batteries included",
                         count: 0
                     },
                     {
                         id: 2,
-                        text: "Java & XML for the win.",
+                        text: "Java (no XML) for the win",
                         count: 0
                     },
                     {
                         id: 3,
-                        text: "I only code functional.",
+                        text: "I love functional",
                         count: 0
                     }
+                ]
+            },
+            {
+                type: 'update',
+                title: 'Have you ever used eval?',
+                id: 2,
+                responses: [
+                    {
+                        id: 0,
+                        text: "Yes, and it was a legitimate use",
+                        count: 0
+                    },
+                    {
+                        id: 1,
+                        text: "Yes, but don't tell anybody",
+                        count: 0
+                    },
+                    {
+                        id: 2,
+                        text: "No, but I was tempted",
+                        count: 0
+                    },
+                    {
+                        id: 3,
+                        text: "No, never needed it",
+                        count: 0
+                    },
+                ]
+            },
+            {
+                type: 'update',
+                title: "What's your opinion on ES6?",
+                id: 3,
+                responses: [
+                    {
+                        id: 0,
+                        text: "I'm thrilled with ES6",
+                        count: 0
+                    },
+                    {
+                        id: 1,
+                        text: "So-so, but love classes",
+                        count: 0
+                    },
+                    {
+                        id: 2,
+                        text: "I hate classes!",
+                        count: 0
+                    },
+                            {
+                                id: 3,
+                                text: "JS's becoming too complex",
+                                count: 0
+                            }
                 ]
             }
         ]
